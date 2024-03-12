22efb1b2083b70faf79c2294e38c3aa66a71a79c
gregkroon <44827446+gregkroon@users.noreply.github.com>
2024-03-12 11:04:21 +1100

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-04e914639d0cca79a"
   instance_type = "t2.micro"
 
   tags = {
